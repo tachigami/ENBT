f5f5f5a7e1c3e2297b9a29fa5bc982ccea9e071a
Vladislavs Golubs <ensirius@ensemplix.ru>
2015-10-01 15:03:44 +0300

    Добавлена возможность компрессии при записи

--- src/main/java/ru/ensemplix/nbt/stream/NBTOutputStream.java
+++ src/main/java/ru/ensemplix/nbt/stream/NBTOutputStream.java
@@ -5,11 +5,16 @@ import ru.ensemplix.nbt.tag.Tag;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class NBTOutputStream extends DataOutputStream {
 
-    public NBTOutputStream(OutputStream out) {
-        super(out);
+    public NBTOutputStream(OutputStream out) throws IOException {
+        this(out, false);
+    }
+
+    public NBTOutputStream(OutputStream out, boolean compressed) throws IOException {
+        super(compressed ? new GZIPOutputStream(out) : out);
     }
 
     public void writeTag(Tag tag) throws IOException {
